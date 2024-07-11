37b5616ac34a7ed23e0deda24047314411eed385
Thomas Lassi <91595715+Lemeard@users.noreply.github.com>
2024-07-11 15:47:13 +0200

    Update befolkningsforandringar_SCB_API.R
    Uppdaterat färgerna som används i skriptet samt ett årligt stapeldiagram som följer utvecklingen per år. Har även lagt till ytterligare en excelfil som ger den årliga förändringen i form av invandringsöverskott, födelseöverskott, flyttnetto inom eget län samt flyttnetto mot övriga Sverige.

--- befolkningsforandringar_SCB_API.R
+++ befolkningsforandringar_SCB_API.R
@@ -18,6 +18,8 @@ library(RColorBrewer)
 library(stringr)
 library(writexl)
 
+source("G:/Avd Strategisk samhällsutveckling/Staben/06 Statistik/5. Thomas/R/skript/diagram_farger.R")
+
 ######## Inställningar ###########################################################################################################################
 geo <- c("1283") # ändra geografi som ska användas här. Helsingborg är exempelvis 1283.                                                        ###
                                                                                                                                                ###
@@ -38,13 +40,13 @@ invandringsöverskott <- c("invandringsöverskott")                  # Invandrin
 utvandring   <- c("utvandringar")                                  # Utvandring                                                                ###
 invandring   <- c("invandringar")                                  # Invandring                                                                ###                                                                                                                                              ###                                                                                                                                    ###                                                                    ###
                                                                                                                                                ###
-utskriftsmapp <- c("C:/Utskrift/") # Utskriftsmapp                                                                                             ###
-bearbetning <- c("Skript av Thomas Lassi, Helsingborgs Stad")      # Till förklaring i nedre högra hörnet                                      ###
+utskriftsmapp <- c("G:/Avd Strategisk samhällsutveckling/Staben/06 Statistik/5. Thomas/Utskrift/") # Utskriftsmapp                             ###                                                                ###
+bearbetning <- c("Skript av Thomas Lassi")      # Till förklaring i nedre högra hörnet                                                         ###
                                                                                                                                                ###
 ##################################################################################################################################################
 
 ### Läser in färger som används längre ner i skriptet 
-farger <- brewer.pal(9, "Blues")[c(TRUE, FALSE)] #Justera och använd lämpliga färger enligt organisationens layout
+farger <- rev(c("#76232F", "#E35205", "#F0B323",  "#C0843D", "#C5B9AC", "lightgrey"))
 
 ### Drar hem statistik från SCB över befolkningsförändringar på månadsbasis
 pxweb_query_list <- 
@@ -81,7 +83,7 @@ for (i in 1:length(variabler)) {
                                       mutate(år = as.numeric(år),
                                              månad = as.numeric(månad)) |> #Försäkrar mig om att både års och månadskolumnerna är numeriska och inte character
                                       filter(region == geo_namn,  #Applicerar filter för att bara ta ut det vi för närvarande är intresserade av
-                                             år>(as.numeric(format(Sys.Date(), "%Y"))-5), # Vi behöver också veta vat dagens datum är
+                                             år>(as.numeric(format(Sys.Date(), "%Y"))-6), # Vi behöver också veta vat dagens datum är
                                              förändringar %in% variabler[i]) |> # Vi vill bara ha variablerna/förändringarna som vi har valt ovan
                                       select(år, månad, Befolkning) |> #Välj kolumnerna vi behöver
                                       group_by(år) |>
@@ -94,9 +96,10 @@ for (i in 1:length(variabler)) {
     #Nedan skapas ett diagram per vald variabel
     kumulativ_plot <-  ggplot(filtrerad_df, aes(x = månad, y = kum_befolkning, color = as.factor(år))) + # den här raden väljer variabler för x och y, samt gruppen som ska bestämma färgerna
                             geom_line(linewidth=1.1)+
+                            geom_point()+
                             scale_color_manual(values = farger) +
                             scale_x_continuous(
-                              limits = c(1, 12),
+                              limits = c(1, 12.1),
                               expand = c(0, 0),
                               breaks = seq(1, 12, by = 1)) +
                             theme(panel.border = element_blank(),
@@ -116,3 +119,52 @@ for (i in 1:length(variabler)) {
 # Skriver ut diagrammet per månad och år (kumulativt)
 ggsave(paste0(utskriftsmapp, "kumulativ","_", variabler[i],"_",paste0(max(px_data_frame$månad)), "_", paste0(unique(px_data_frame$region)), ".png"), kumulativ_plot,width = 7,height=4, dpi=300, units = "in")
 }
+
+### Befolkningsförändringar per år i ett stapeldiagram
+
+## Följande kod skapar en ny dataframe i det format som vi behöver för att kunna skapa stapeldiagrammet
+flyttnetto_df <- excel_output |> 
+                    select(månad, födelseöverskott, `flyttningsöverskott eget län`, `flyttningsöverskott övriga Sverige`, invandringsöverskott) |> 
+                    separate(månad, into = c("år", "månad"), sep = "M", convert = TRUE) |>
+                    mutate(år = as.numeric(år),
+                           månad = as.numeric(månad)) |> 
+                    select(-månad) |> 
+                    group_by(år) |> 
+                    summarise(across(everything(), sum, .names = "sum_{.col}")) |> 
+                    rename(Födelseöverskott = sum_födelseöverskott,
+                           "Flyttnetto eget län" = `sum_flyttningsöverskott eget län`,
+                           "Flyttnetto övriga sverige" = `sum_flyttningsöverskott övriga Sverige`,
+                           "Invandringsöverskott" = sum_invandringsöverskott) |> 
+                    pivot_longer(-år, names_to = "Typ", values_to = "Antal")
+
+## Skriver ut ett exceldokument med förändringarna i utskriftsmappen som har angivits tidigare
+write_xlsx(flyttnetto_df,paste0(utskriftsmapp, "Flyttnetto", geo_namn, "_",max(px_data_frame$månad),".xlsx")) # Skriver ut själva excel-filen
+
+## Skapar själva stapeldiagrammet
+flyttnetto <- ggplot(flyttnetto_df, aes(fill = Typ, y=Antal, x=år)) + 
+                          geom_bar(position="stack", stat="identity", color="black") +
+                          scale_fill_manual(values = c("#F0B323", "#E35205", "#C0843D", "#C5B9AC"))+
+                          scale_x_continuous(
+                            limits = c(min(flyttnetto_df$år)-1, max(flyttnetto_df$år)+1),
+                            expand = c(0, 0),
+                            breaks = seq(min(flyttnetto_df$år), max(flyttnetto_df$år), by = 2))+
+                          theme(panel.border = element_blank(),
+                                axis.line = element_line(color = 'black'),
+                                legend.position = "bottom",
+                                panel.background = element_rect(fill = "white"),
+                                legend.key = element_blank(),
+                                legend.title = element_blank(),
+                                panel.grid.major.y = element_line(color = "lightgray", linewidth = 0.3),
+                                axis.ticks.length.y = unit(0, "mm"),
+                                axis.ticks.length.x = unit(2, "mm"),
+                                plot.title = element_text(hjust = 0.5),
+                                axis.title = element_blank(),
+                                axis.text.x = element_text(angle = 0, vjust = 0.9, hjust=0.5, size=8),
+                                plot.caption = element_text(size = 5))+
+                          labs(title = paste0("Befolkningsförändringar för ", unique(excel_output$region), " (", min(flyttnetto_df$år), "-", max(flyttnetto_df$år),")"),
+                               x = "År",
+                               y = "Antal",
+                               caption = paste0("Förklaring: Befolkningsförändringar för ", unique(excel_output$region), " (", min(flyttnetto_df$år), "-", max(flyttnetto_df$år),") uppdelat på typ av förändring.\nBearbetning efter skript av Thomas Lassi\nKälla: SCB"))
+
+## Skriver ut stapeldiagrammet i utskriftsmappen
+ggsave(paste0(utskriftsmapp, "flyttnetto","_",paste0(max(px_data_frame$månad)), "_", paste0(unique(px_data_frame$region)), ".png"), flyttnetto,width = 7,height=4, dpi=300, units = "in")
